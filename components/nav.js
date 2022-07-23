9e4172556a22e3b51a77b8ae8267653ee276fe3b
Allen <happyalien543@gmail.com>
2022-07-23 14:35:28 +0800

    5

--- components/nav.js
+++ components/nav.js
@@ -34,6 +34,7 @@ const nav = () => {
                             </li>
                         </ul>
                     </div>
+
                     <div className={style.user}>
                         <Link href="">
                             <a>
