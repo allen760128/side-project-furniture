733343ce9d3a0fbb99ed1c7abaa2f6fcd1b653ba
Allen <happyalien543@gmail.com>
2022-07-30 22:24:13 +0800

    20220730 update

--- components/nav.js
+++ components/nav.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import style from './nav.module.css';
 import Link from 'next/link';
 
 const nav = () => {
+    const [rwd, setRwd] = useState(false);
+
+    const handleRwdMenu = () => {
+        setRwd(true);
+    }
+
+    const handleRwdClose = () => {
+        setRwd(false);
+    }
+
+    useEffect(() => {
+        window.onresize = () => {
+            setRwd(false);
+        }
+    }, []);
+
     return (
         <div className={style.nav}>
             <div className={style.navWrap}>
@@ -45,10 +61,47 @@ const nav = () => {
                             <a>
                                 <span style={{ backgroundImage: 'url(/assets/nav_icon2.svg)' }}></span>
                             </a>
-                        </Link> </div>
+                        </Link>
+                        <Link href="">
+                            <a onClick={handleRwdMenu}>
+                                <span style={{ backgroundImage: 'url(/assets/nav_icon3.svg)' }}></span>
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div className={style.rwdMd}
+                style={{ left: rwd ? '0' : '-100%' }}
+                onClick={handleRwdClose}
+            >
+                <div className={style.rwdList} onClick={(e) => { e.stopPropagation() }}
+                    style={{ left: rwd ? '0' : '-100%' }}>
+                    <ul className={style.rwdlistUl}>
+                        <li>
+                            <Link href="">
+                                <a>Product</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="">
+                                <a>Services</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="">
+                                <a>Article</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="">
+                                <a>About Us</a>
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
+
     )
 }
 
