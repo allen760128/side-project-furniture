0b1ed5773b6b3a53f4c5081570f43b172b5a4906
Allen <happyalien543@gmail.com>
2022-08-03 23:08:10 +0800

    20220803 update product

---
+++ pages/data/productsData.js
@@ -0,0 +1,66 @@
+export const item = [
+    {
+        id: 1,
+        pic: '/assets/products_pic1.jpg',
+        sub1: 'Chair',
+        title: 'White Aesthetic Chair',
+        sub2: 'Combination of wood and wool',
+        price: '$63.47'
+    }, {
+        id: 2,
+        pic: '/assets/products_pic2.jpg',
+        sub1: 'Cupboard',
+        title: 'Wooden Cupboard 3 Row',
+        sub2: 'Combination of wood and wool',
+        price: '$79.88'
+    }, {
+        id: 3,
+        pic: '/assets/products_pic3.jpg',
+        sub1: 'Chair',
+        title: 'Minimalist Lounge Chair',
+        sub2: 'Combination of wood and wool',
+        price: '$14.74'
+    }, {
+        id: 4,
+        pic: '/assets/products_pic4.jpg',
+        sub1: 'Table',
+        title: 'Working Desk Setup',
+        sub2: 'Combination of wood and wool',
+        price: '$47.90'
+    }, {
+        id: 5,
+        pic: '/assets/products_pic5.jpg',
+        sub1: 'Cupboard',
+        title: 'Minimalist White Cuppboard',
+        sub2: 'Combination of wood and wool',
+        price: '$40.73'
+    }, {
+        id: 6,
+        pic: '/assets/products_pic6.jpg',
+        sub1: 'Table',
+        title: 'Wooden Dining Table',
+        sub2: 'Combination of wood and wool',
+        price: '$16.50'
+    }, {
+        id: 7,
+        pic: '/assets/products_pic7.jpg',
+        sub1: 'Decoration',
+        title: 'White Minimalist Vase',
+        sub2: 'Combination of wood and wool',
+        price: '$58.39'
+    }, {
+        id: 8,
+        pic: '/assets/products_pic8.jpg',
+        sub1: 'Decoration',
+        title: 'Plant With Clay Stand',
+        sub2: 'Combination of wood and wool',
+        price: '$61.49'
+    }, {
+        id: 9,
+        pic: '/assets/products_pic9.jpg',
+        sub1: 'Decoration',
+        title: 'Oval Gold Mirror',
+        sub2: 'Combination of wood and wool',
+        price: '$32.43'
+    },
+];
--- pages/products.js
+++ pages/products.js
@@ -3,11 +3,15 @@ import style from '../styles/products.module.css';
 import Head from '../components/head';
 import Nav from '../components/nav';
 import Footer from '../components/footer';
+import Image from 'next/image';
 import Link from 'next/link';
 import Carousell from '../components/carousell'
+import { item } from './data/productsData'
 // import { Aa } from '../styles/aa.js';
 
 const products = () => {
+
+
     return (
         <div>
             <Head title={'Lalasia products'}></Head>
@@ -32,6 +36,38 @@ const products = () => {
                         <span style={{ backgroundImage: 'url(/assets/products_search_icon1.svg)' }}></span>
                         Filter
                     </button>
+                </div>
+            </div>
+            <div className={`${style.products}  ${style.wrap}`}>
+                <div className={style.title}>
+                    <div className={style.left}>
+                        <h2>Total Product</h2>
+                        <span>184</span>
+                    </div>
+                    <div className={style.right}>
+                        <span style={{ backgroundImage: 'url(/assets/products_icon1.svg)' }}></span>
+                        <p>Sort By</p>
+                    </div>
+                </div>
+                <div className={style.list}>
+                    <ul className={style.listUl}>
+                        {
+                            item.map(data => (
+                                <li key={data.id}>
+                                    <Link href='/#'>
+                                        <a>
+                                            <Image src={data.pic} alt='banner' width={394} height={360} layout='responsive'></Image >
+                                        </a>
+                                    </Link>
+                                    <h5>{data.sub1}</h5>
+                                    <h3>{data.title}</h3>
+                                    <p>{data.sub2}</p>
+                                    <h3>{data.price}</h3>
+                                </li>
+                            ))
+                        }
+
+                    </ul>
                 </div>
             </div>
             <Footer></Footer>
