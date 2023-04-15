1658beaa8396372df99f6a8cc5901936b0b79525
Allen <happyalien543@gmail.com>
2023-04-15 16:15:02 +0800

    scss

---
+++ pages/test/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './test.module.scss'
+
+const Test = () => {
+    return (
+        <div className={style.con}>
+            <div className={style.main}>
+                <p>sadf</p>
+            </div>
+        </div>
+    )
+}
+
+export default Test;
